6e60d0fdaf4746c72a099eda45073835959987af
shmlkv <splo1t@icloud.com>
2018-01-22 07:59:37 +0300

    🆕 Mobile First DatePicker

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
 export function matchXs() {
-  return window.matchMedia("(max-width: 450px)").matches;
+  return window.matchMedia("(max-width: 575px)").matches;
+}
+
+export function matchMd() {
+  return window.matchMedia("(max-width: 991px)").matches;
 }
